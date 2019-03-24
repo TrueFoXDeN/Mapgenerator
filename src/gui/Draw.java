d27fcb6bc639fef85cb667eefeb64d6a522baa54
steven <steven.tepe@web.de>
2019-03-24 01:29:15 +0100

    Added Draw Class

---
+++ src/gui/Draw.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Draw extends JLabel {
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        repaint();
+    }
+
+}
